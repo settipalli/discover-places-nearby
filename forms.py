2448f6b26b9665d28a7cb85e24d46822705bc411
Santhoshkumar Settipalli <santhosh.settipalli@gmail.com>
2017-08-12 20:18:22 +0530

    Added signup form backend.

--- forms.py
+++ forms.py
@@ -1,9 +1,9 @@
 from flask_wtf import Form
-from wtforms import StringField, PasswordFiled, SubmitField
+from wtforms import StringField, PasswordField, SubmitField
 
 class SignupForm(Form):
     first_name = StringField('First name')
     last_name = StringField('Last name')
     email = StringField('Email')
-    password = PasswordFiled('Password')
+    password = PasswordField('Password')
     submit = SubmitField('Sign up')
