51fe3626f30122f9b8c09665ee15c5fb950b14e5
Santhoshkumar Settipalli <santhosh.settipalli@gmail.com>
2017-08-13 03:55:24 +0530

    Authorization.

--- routes.py
+++ routes.py
@@ -25,6 +25,9 @@ def about():
 
 @app.route("/signup", methods=["GET", "POST"])
 def signup():
+    if "email" in session:
+        return redirect(url_for("home"))
+
     form = SignupForm()
 
     if request.method == "GET":
@@ -44,11 +47,17 @@ def signup():
 
 @app.route("/home")
 def home():
+    if "email" not in session:
+        return redirect(url_for("login"))
+
     return render_template("home.html")
 
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
+    if "email" in session:
+        return redirect(url_for("home"))
+
     form = LoginForm()
 
     if request.method == "GET":
