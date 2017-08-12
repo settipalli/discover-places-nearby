630b9872e1deb88fa5249cd3585a831ab1581e25
Santhoshkumar Settipalli <santhosh.settipalli@gmail.com>
2017-08-13 01:36:14 +0530

    Password length validation.

--- forms.py
+++ forms.py
@@ -1,11 +1,11 @@
 from flask_wtf import Form
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired # basic data validator
-from wtforms.validators import Email
+from wtforms.validators import Email, Length
 
 class SignupForm(Form):
     first_name = StringField('First name', validators=[DataRequired("Please enter your first name.")])
     last_name = StringField('Last name', validators=[DataRequired("Please enter your last name.")])
     email = StringField('Email', validators=[DataRequired("Please enter your email."), Email("Please enter a valid email address.")])
-    password = PasswordField('Password', validators=[DataRequired("Please enter your password.")])
+    password = PasswordField('Password', validators=[DataRequired("Please enter your password."), Length(min=8, message="Your password should be 8 characters or more.")])
     submit = SubmitField('Sign up')
