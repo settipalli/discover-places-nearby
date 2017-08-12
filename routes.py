0dfc6bd237885363ecb4654945fbb5fb27ae6d63
Santhoshkumar Settipalli <santhosh.settipalli@gmail.com>
2017-08-13 01:06:54 +0530

    Added ability to differentiate between GET and POST request.

--- routes.py
+++ routes.py
@@ -1,6 +1,6 @@
 # Will contain the applications main code.
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from models import db
 from forms import SignupForm
 
@@ -12,18 +12,26 @@ db.init_app(app)
 # Prevent CSRF attack (form)
 app.secret_key = "development-key"
 
+
 @app.route("/")
 def index():
     return render_template("index.html")
+
 
 @app.route("/about")
 def about():
     return render_template("about.html")
 
-@app.route("/signup")
+
+@app.route("/signup", methods=["GET", "POST"])
 def signup():
-    form = SignupForm()
-    return render_template("signup.html", form=form)
+    if request.method == "GET":
+        form = SignupForm()
+        return render_template("signup.html", form=form)
+
+    elif request.method == "POST":
+        return "Success!"
+
 
 if __name__ == "__main__":
     app.run(debug=True)
