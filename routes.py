8463f3c522baccdf1d0ec55e9f769978f9a8694e
Santhoshkumar Settipalli <santhosh.settipalli@gmail.com>
2017-08-12 20:01:54 +0530

    Connected the application to the local postgresql database.

--- routes.py
+++ routes.py
@@ -4,6 +4,8 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/locationbasedservice'
+
 @app.route("/")
 def index():
     return render_template("index.html")
