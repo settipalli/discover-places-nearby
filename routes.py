5b29e04d6c7326807b8bba560dcb0ea473948a16
Santhoshkumar Settipalli <santhosh.settipalli@gmail.com>
2017-08-13 03:19:36 +0530

    Added support for session.

--- routes.py
+++ routes.py
@@ -1,6 +1,6 @@
 # Will contain the applications main code.
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect, url_for
 from models import db, User
 from forms import SignupForm
 
@@ -37,8 +37,14 @@ def signup():
             user = User(form.first_name.data, form.last_name.data, form.email.data, form.password.data)
             db.session.add(user)
             db.session.commit()
-            return "Success!"
 
+            session["email"] = user.email
+            return redirect(url_for("home"))
+
+
+@app.route("/home")
+def home():
+    return render_template("home.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
